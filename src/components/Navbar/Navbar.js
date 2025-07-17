7600439f0f77053cf82191ae0131c69e28341c28
Kishore771 <kishoregundelli156@gmail.com>
2025-07-17 22:20:28 +0530

    changes made in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,40 +1,43 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './Navbar.css'; // Optional for custom styling
+import './Navbar.css'; // Optional: for custom styles
 
-const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-    <div className="container">
-      <a className="navbar-brand" href="/">
-        <img
-          src="https://i.postimg.cc/Zn5nwXTf/logo-transparent-png.png"
-          alt="logo"
-          className="food-munch-logo"
-        />
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img
+            src="https://i.postimg.cc/Zn5nwXTf/logo-transparent-png.png"
+            alt="logo"
+            className="food-munch-logo"
+          />
+        </a>
+        
+        {/* Updated for Bootstrap 5 */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      {/* Key Change: justify-content-end to align items right */}
-      <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <a className="nav-link active" href="#wcuSection">Why Choose Us?</a>
-          <a className="nav-link" href="#exploreMenuSection">Explore Menu</a>
-          <a className="nav-link" href="#deliveryPaymentSection">Delivery & Payment</a>
-          <a className="nav-link" href="#followUsSection">Follow Us</a>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-link active" href="#wcuSection">Why Choose Us?</a>
+            <a className="nav-link" href="#exploreMenuSection">Explore Menu</a>
+            <a className="nav-link" href="#deliveryPaymentSection">Delivery & Payment</a>
+            <a className="nav-link" href="#followUsSection">Follow Us</a>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default Navbar;
